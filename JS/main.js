4b1524399be90f7e7e0dd7c389bef8ced7465dec
Diego Ferreyra <diegofacundoferreyra@gmail.com>
2025-09-24 20:58:31 -0300

    Terminado para arrancar con hoja de JS

--- JS/main.js
+++ JS/main.js
@@ -1,24 +1,12 @@
 /* Clase que modela y define las bicicletas */
 class Bicis {
-  /**
-   *
-   * @param {*} id
-   * @param {*} clase
-   * @param {*} rodado
-   * @param {*} precio
-   */
-
   constructor(id, clase, rodado, precio) {
     this.id = id;
     this.clase = clase;
     this.rodado = rodado;
     this.precio = precio;
   }
-  /*FUNCION PARA MOSTRAR LA DESCRIPCION COMPLETA DE LA BICI */
-  /**
-   *
-   * @returns
-   */
+
   mostrarDescripcionCompleta() {
     return (
       "#" +
@@ -28,7 +16,7 @@ class Bicis {
       " Rodado: " +
       this.rodado +
       " $ " +
-      this.precio
+      this.precio.toLocaleString("es-AR")
     );
   }
 
@@ -58,7 +46,7 @@ class Bicis {
   }
 }
 
-//BASE DE DATOS DE LAS BIBICLETAS
+// BASE DE DATOS DE LAS BICICLETAS
 const arregloBicis = new Array();
 arregloBicis.push(new Bicis(1, "Carrera", 29, 500000));
 arregloBicis.push(new Bicis(2, "Montaña", 28, 420000));
@@ -66,27 +54,34 @@ arregloBicis.push(new Bicis(3, "Paseo", 26, 280000));
 arregloBicis.push(new Bicis(4, "Playera", 26, 200000));
 arregloBicis.push(new Bicis(5, "Niños", 16, 150000));
 arregloBicis.push(new Bicis(6, "Plegable", 20, 340000));
-let respuesta = true;
 
-// SI NO FUNCA, CABMBIAR EL 4 DE ABAJO POR SALIR
-while (respuesta != "4" && respuesta) {
+let respuesta = true;
+while (respuesta != "5" && respuesta) {
   respuesta = mostrarMenu();
 }
 
 function mostrarMenu() {
   let respuesta = prompt(
-    "Elija una opcion: \n 1) Ver las bicicletas \n 2) Asignar nuevo precio \n 3) Ordenar por precio \n 4) Salir"
+    "Elija una opcion:\n" +
+      "1) Ver las bicicletas\n" +
+      "2) Asignar nuevo precio\n" +
+      "3) Ordenar por precio\n" +
+      "4) Buscar por clase\n" +
+      "5) Salir"
   );
+
   if (respuesta == "1") {
-    alert("Las bicicletas son: \n " + mostrarStock());
+    alert("Las bicicletas son:\n" + mostrarStock());
   } else if (respuesta == "2") {
     actualizarPrecio();
   } else if (respuesta == "3") {
     ordenarPorPrecio();
-  } else if (respuesta.toLocaleLowerCase() == "4") {
+  } else if (respuesta == "4") {
+    buscarPorClase();
+  } else if (respuesta.toLowerCase() == "5") {
     return respuesta.toLowerCase();
   } else {
-    alert("Opcion invalida");
+    alert("Opción inválida");
   }
   return respuesta;
 }
@@ -110,10 +105,10 @@ function mostrarStock() {
   return stock;
 }
 
-//FORMA DE HACERLO DE MENOR A MAYOR
+// ORDENAR DE MENOR A MAYOR
 function ordenarPorPrecio() {
   arregloBicis.sort((a, b) => a.getPrecio() - b.getPrecio());
-  alert("Las bicicletas ordenadas por precio son: \n" + mostrarStock());
+  alert("Las bicicletas ordenadas por precio son:\n" + mostrarStock());
 }
 
 //FORMA DE HACERLO DE MAYOR A MENOR
@@ -122,14 +117,41 @@ function ordenarPorPrecio() {
   alert("Las bicicletas ordenadas por precio son: \n" + mostrarStock());
 } */
 
+// ACTUALIZAR PRECIO
 function actualizarPrecio() {
-  let bici = prompt("Ingrese la bicicleta a actualizar");
+  let bici = Number(prompt("Ingrese el ID de la bicicleta a actualizar"));
+  if (isNaN(bici)) {
+    alert("Debe ingresar un número válido");
+    return;
+  }
+
   let indice = buscarBici(bici);
   if (indice >= 0) {
-    let nuevo_precio = prompt("Ingrese un nuevo precio");
-    arregloBicis[indice].setPrecio(nuevo_precio);
-    alert("Ahora la lista es: \n" + mostrarStock());
+    let nuevo_precio = Number(prompt("Ingrese un nuevo precio"));
+    if (!isNaN(nuevo_precio) && nuevo_precio > 0) {
+      arregloBicis[indice].setPrecio(nuevo_precio);
+      alert("Ahora la lista es:\n" + mostrarStock());
+    } else {
+      alert("Precio inválido. Debe ser un número mayor a 0.");
+    }
   } else {
-    alert("No ingresaste una bicicleta en stock");
+    alert("No ingresaste un ID de bicicleta válido en stock");
+  }
+}
+
+// BUSCAR POR CLASE
+function buscarPorClase() {
+  let clase = prompt("Ingrese la clase de bicicleta a buscar:").toLowerCase();
+  let resultado = arregloBicis.filter(
+    (b) => b.getClase().toLowerCase() === clase
+  );
+
+  if (resultado.length > 0) {
+    alert(
+      "Resultados:\n" +
+        resultado.map((b) => b.mostrarDescripcionCompleta()).join("\n")
+    );
+  } else {
+    alert("No se encontraron bicicletas de esa clase");
   }
 }
